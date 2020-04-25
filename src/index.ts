7f621c956b861e5ece911ac6ee326d5de6ab1838
Ricky Chien <rickychien@icloud.com>
2020-04-26 00:04:35 +0900

    Push test ping ws server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import http from 'http'
+import express from 'express'
 import WebSocket from 'ws'
 import SignalingService from './signaling.js'
 
 const PORT = 5001
-const server = http.createServer()
-const wss = new WebSocket.Server({ server })
+const app = express()
 const signalingService = new SignalingService()
 
+app.use(express.static('/'))
+const server = http.createServer(app)
+server.listen(PORT)
+console.log('Server is listening on port %d', PORT)
+
+const wss = new WebSocket.Server({ server })
+
 wss.on('connection', function (ws) {
+  const id = setInterval(function () {
+    ws.send(JSON.stringify(new Date()), function () {})
+  }, 1000)
+
   ws.on('open', function () {
     signalingService.addClient(ws)
   })
 
   ws.on('close', function () {
+    clearInterval(id)
     signalingService.deleteClient(ws)
   })
 
@@ -22,6 +34,3 @@ wss.on('connection', function (ws) {
     }
   })
 })
-
-server.listen(PORT)
-console.log('Server is listening on port %d', PORT)
