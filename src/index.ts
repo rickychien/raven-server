2152c5b81b83412f82701f6f7dd2a53aebeeb2d1
Ricky Chien <rickychien@icloud.com>
2020-04-26 00:18:33 +0900

    Customize server port

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,26 @@
-import http from 'http'
-import express from 'express'
 import WebSocket from 'ws'
 import SignalingService from './signaling.js'
 
-const PORT = 5001
-const app = express()
+const port = Number(process.env.PORT) || 5001
 const signalingService = new SignalingService()
-
-app.use(express.static('/'))
-const server = http.createServer(app)
-server.listen(PORT)
-console.log('Server is listening on port %d', PORT)
-
-const wss = new WebSocket.Server({ server })
+const wss = new WebSocket.Server({ port }, () => {
+  console.info(`Signaling server is running on port ${port}.`)
+})
 
 wss.on('connection', function (ws) {
-  const id = setInterval(function () {
-    ws.send(JSON.stringify(new Date()), function () {})
-  }, 1000)
-
   ws.on('open', function () {
     signalingService.addClient(ws)
   })
 
   ws.on('close', function () {
-    clearInterval(id)
     signalingService.deleteClient(ws)
   })
 
   ws.on('message', function (message) {
     if (typeof message === 'string') {
       signalingService.onMessage(ws, message)
+    } else {
+      console.info(`Detected an unknown ${typeof message} message.`)
     }
   })
 })
